272547fbe65c8e8735e15824dfb5ed13672d8e1e
Michael Cote <michaelcote7@outlook.com>
2022-12-28 14:57:29 -0800

    new

--- js.js
+++ js.js
@@ -1,10 +1,10 @@
-const contacts = new Map()
-contacts.set('Jessie', {phone: "213-555-1234", address: "123 N 1st Ave"})
-contacts.has('Jessie') // true
-console.log(contacts.get('Jessie'))
-contacts.get('Hilary') // undefined
-contacts.set('Hilary', {phone: "617-555-4321", address: "321 S 2nd St"})
-contacts.get('Jessie') // {phone: "213-555-1234", address: "123 N 1st Ave"}
-contacts.delete('Raymond') // false
-contacts.delete('Jessie') // true
-// console.log(contacts.size) // 1
+// const contacts = new Map()
+// contacts.set('Jessie', {phone: "213-555-1234", address: "123 N 1st Ave"})
+// contacts.has('Jessie') // true
+// console.log(contacts.get('Jessie'))
+// contacts.get('Hilary') // undefined
+// contacts.set('Hilary', {phone: "617-555-4321", address: "321 S 2nd St"})
+// contacts.get('Jessie') // {phone: "213-555-1234", address: "123 N 1st Ave"}
+// contacts.delete('Raymond') // false
+// contacts.delete('Jessie') // true
+// // console.log(contacts.size) // 1
---
+++ two.js
@@ -0,0 +1,12 @@
+
+// const number = 212121;
+// const removeDigit = (num, remove) => {
+//     const arr = +Array
+//         .from(String(num), Number)
+//         .filter((n) => n !== remove)
+//         .toString()
+//         .split(',')
+//         .join('');
+//     console.log(arr)
+// };
+// removeDigit(number, 2);
